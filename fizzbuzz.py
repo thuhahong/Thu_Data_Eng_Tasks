5867f152008bdc3972b5dee548dbd975e5c1c7dd
thu_h <thu_ha_hong_tech@outlook.com>
2021-03-11 17:22:24 +0000

    function, for loop, if, elif and else statements

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for number in range(20):
+        if number % 3 == 0 and number % 5 == 0:
+            print("fizzbuzz")
+        elif number % 3 == 0:
+            print("fizz")
+        elif number % 5 == 0:
+            print("fizz")
+        else:
+            print(number)
+
+
+fizzbuzz()
