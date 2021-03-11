6536baf54867354311d1cc48a4a507e2837f98ff
thu_h <thu_ha_hong_tech@outlook.com>
2021-03-11 17:37:25 +0000

    formatting information using f-string, input, variables and print

---
+++ collect_user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+age = input("How old are you?")
+address = input("Which city do you live in?")
+favourite_film = input("What is your favourite film?")
+
+print(f"Hi, my name is {name}. I am {age} years old. I live in {address}. My favourite film is {favourite_film}.")
